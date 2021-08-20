6aa9ad225f38206892aa0f3462a90e8f5faa15e6
DanishPandita <danishpandita1999@gmail.com>
2021-08-20 19:30:54 +0530

    commit2

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 /** @format */
+import ProgressBar from "@badrap/bar-of-progress";
 import "@material-tailwind/react/tailwind.css";
 import { Provider } from "next-auth/client";
 import Head from "next/head";
+import Router from "next/router";
 import "tailwindcss/tailwind.css";
+const progress = new ProgressBar({
+  size: 3,
+  color: "#00C1D4",
+  className: "z-50",
+  delay: 100,
+});
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -12,7 +23,6 @@ export default function App({ Component, pageProps }) {
           rel="stylesheet"
         />
       </Head>
-
       <Provider session={pageProps.session}>
         <Component {...pageProps} />
       </Provider>
