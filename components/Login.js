88604cfa198435002b216044d8dd800c0df201b5
Danish Pandita <49803908+DANISHPANDITA@users.noreply.github.com>
2021-08-20 21:44:18 +0530

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -6,7 +6,7 @@ function Login() {
   const logo =
     "https://www.dignited.com/wp-content/uploads/2020/04/Google-Docs-Header-1280x720-1-1024x576.png";
   return (
-    <div className="w-1/3 mx-auto">
+    <div className="w-1/3 mx-auto xs:w-2/3">
       <img src={logo} alt="" className="h-36 w-auto mx-auto mt-64" />
       <Button
         color="lightBlue"
